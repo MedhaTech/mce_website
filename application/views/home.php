f376955fd7b6278634b7b42fa1939e0cf7c41cdf
pramod1496 <pramod@medhatech.in>
2025-06-26 11:47:29 +0530

    slider

--- application/views/home.php
+++ application/views/home.php
@@ -115,11 +115,11 @@
 
           </li>
 
-          <li data-index="rs-12" data-transition="slidingoverlayhorizontal" data-slotamount="default" data-easein="default" data-easeout="default" data-masterspeed="default" data-thumb="<?php echo base_url(); ?>assets/images/slider/SPOCC.png" data-rotate="0" data-saveperformance="off" data-title="Slide 12" data-description="">
+          <!-- <li data-index="rs-12" data-transition="slidingoverlayhorizontal" data-slotamount="default" data-easein="default" data-easeout="default" data-masterspeed="default" data-thumb="<?php echo base_url(); ?>assets/images/slider/SPOCC.png" data-rotate="0" data-saveperformance="off" data-title="Slide 12" data-description="">
             
             <img src="<?php echo base_url(); ?>assets/images/slider/SPOCC.png" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-bgparallax="10" data-no-retina>
 
-          </li>
+          </li> -->
 
 
         <!-- <li data-index="rs-11" data-transition="slidingoverlayhorizontal" data-slotamount="default" data-easein="default" data-easeout="default" data-masterspeed="default" data-thumb="<?php echo base_url(); ?>assets/images/iicccd.png" data-rotate="0" data-saveperformance="off" data-title="Slide 11" data-description="">
